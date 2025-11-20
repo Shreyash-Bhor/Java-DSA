ebfa54dc410999b9befb47446c90d9ab54ba5596
Shreyash Bhor <shreyeshbhor@gmail.com>
2025-11-20 07:39:58 +0530

    Bubble Sort

---
+++ Sorts/BubbleSort.java
@@ -0,0 +1,45 @@
+/* Date: 20-11-2025 
+* Bubble Sort Algorithm
+*/
+
+package Sorts;
+
+import java.util.Scanner;
+
+public class BubbleSort {
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printArr(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println();
+        System.out.println("Enter number of elements : ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Enter element " + (i + 1) + ":");
+            arr[i] = sc.nextInt();
+        }
+        bubbleSort(arr);
+        printArr(arr);
+        sc.close();
+    }
+}
