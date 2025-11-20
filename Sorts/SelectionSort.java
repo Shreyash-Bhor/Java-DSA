c98523bd8c235b6b5579afc266c014ab8d8789e3
Shreyash Bhor <shreyeshbhor@gmail.com>
2025-11-20 08:14:37 +0530

    Selection Sort

---
+++ Sorts/SelectionSort.java
@@ -0,0 +1,45 @@
+/* Date: 20-11-2025 
+* Selection Sort Algorithm
+*/
+
+package Sorts;
+
+import java.util.*;
+
+public class SelectionSort {
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minPos = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[minPos] > arr[j]) {
+                    minPos = j;
+                }
+            }
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void printArr(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println();
+        System.out.println("Enter number of elements : ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Enter element " + (i + 1) + ":");
+            arr[i] = sc.nextInt();
+        }
+        selectionSort(arr);
+        printArr(arr);
+        sc.close();
+    }
+}
