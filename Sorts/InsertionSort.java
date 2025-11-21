f6b8aaf9f563b5c724c80f12746e3eb956f059ac
Shreyash Bhor <shreyeshbhor@gmail.com>
2025-11-21 18:18:41 +0530

    Solved Insertion Sort

---
+++ Sorts/InsertionSort.java
@@ -0,0 +1,45 @@
+/* Date: 21-11-2025 
+* Insertion Sort Algorithm
+*/
+
+package Sorts;
+
+import java.util.Scanner;
+
+public class InsertionSort {
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int curr = arr[i];
+            int prev = i - 1;
+
+            while (prev >= 0 && arr[prev] > curr) {
+                arr[prev + 1] = arr[prev];
+                prev--;
+            }
+            arr[prev + 1] = curr;
+        }
+
+    }
+
+    public static void printArr(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println();
+        System.out.println("Enter number of elements : ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Enter element " + (i + 1) + ":");
+            arr[i] = sc.nextInt();
+        }
+        insertionSort(arr);
+        printArr(arr);
+        sc.close();
+    }
+}
