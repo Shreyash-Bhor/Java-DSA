573b1984e147b287cb57601afcd428b98edf730f
Shreyash Bhor <shreyeshbhor@gmail.com>
2025-11-25 07:24:19 +0530

    Typo in Count sort

--- Sorts/CountSort.java
+++ Sorts/CountSort.java
@@ -10,7 +10,7 @@ public class CountSort {
     public static void countSort(int[] arr) {
         int largest = Integer.MIN_VALUE;
         for (int i = 0; i < arr.length; i++) {
-            largest = Math.max(largest, i);
+            largest = Math.max(largest, arr[i]);
         }
         int[] count = new int[largest + 1];
         for (int i = 0; i < arr.length; i++) {
