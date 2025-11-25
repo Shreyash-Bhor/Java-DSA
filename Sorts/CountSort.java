aabb30e6a50846191d6cbf78bde49e7eb814228c
Shreyash Bhor <shreyeshbhor@gmail.com>
2025-11-25 08:23:04 +0530

    Sorted array in reverse order

--- Sorts/CountSort.java
+++ Sorts/CountSort.java
@@ -28,6 +28,27 @@ public class CountSort {
         }
     }
 
+    public static void reverseCountSort(int[] arr) {
+        int largest = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            largest = Math.max(largest, arr[i]);
+        }
+        int[] count = new int[largest + 1];
+        for (int i = 0; i < arr.length; i++) {
+            count[arr[i]]++;
+        }
+        int j = 0;
+        for (int i = count.length - 1; i >= 0; i--) {
+
+            while (count[i] > 0) {
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+
+        }
+    }
+
     public static void printArr(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
@@ -46,6 +67,8 @@ public class CountSort {
             arr[i] = sc.nextInt();
         }
         countSort(arr);
+        printArr(arr);
+        reverseCountSort(arr);
         printArr(arr);
         sc.close();
     }
