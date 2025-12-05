176bc95060f8ec8f270859aa24dd79b7a74ec4a2
Shreyash Bhor <shreyeshbhor@gmail.com>
2025-12-05 07:58:38 +0530

    Solved all bitwise operations

--- BitManipulation/BitOperations.java
+++ BitManipulation/BitOperations.java
@@ -33,11 +33,27 @@ public class BitOperations {
         return setBit(n, i);
     }
 
+    // clear i bits
+    public static int clearIBits(int n, int i) {
+        int bitmask = (~0) << i;
+        return n & bitmask;
+    }
+
+    // clear i bits in range
+    public static int clearIBitsInRange(int n, int i, int j) {
+        int a = ((~0) << (j + 1)); // 11111000000 Left side all ones.
+        int b = (1 << i) - 1; // Right side all ones , if i=3 then 0111
+        int bitmask = a | b;
+        return n & bitmask;
+    }
+
     public static void main(String[] args) {
         System.out.println();
         System.out.println(getBit(10, 2)); // 0
         System.out.println(setBit(10, 2)); // 14
         System.out.println(clearBit(10, 1)); // 8
         System.out.println(updateBit(10, 2, 1)); // 14
+        System.out.println(clearIBits(15, 2)); // 12
+        System.err.println(clearIBitsInRange(10, 2, 4)); // 2
     }
 }
